447e7ea25e7007f5a724e3f96163ec0ba9ac5ff9
Ilya Ivanishin <ilyaivanish@gmail.com>
2023-03-16 16:53:47 +0200

    home-work-objects-in-details

--- home-work-objects-in-details.js
+++ home-work-objects-in-details.js
@@ -1,29 +1,25 @@
 // 1. Задача про обчислення різниці часу
 
-function durationBetweenDates(startDate = '24 Aug 1991', endDate = '24 Aug 1991', dimension = 'days') {
-  const start = new Date(startDate);
-  const end = new Date(endDate);
+function durationBetweenDates(firstDate = '24 Aug 1991', secondDate = '24 Aug 1991', dimension = 'days') {
+  const startDate = new Date(firstDate);
+  const endDate = new Date(secondDate);
 
-  const timeDiff = Math.abs(end - start); // time difference in milliseconds
+  const timeDiff = Math.abs(endDate - startDate); // time difference in milliseconds
 
-  switch (dimension) {
-    case 'days':
-      const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-      return `${daysDiff} days`;
-    case 'hours':
-      const hoursDiff = Math.floor(timeDiff / (1000 * 60 * 60));
-      return `${hoursDiff} hours`;
-    case 'minutes':
-      const minutesDiff = Math.floor(timeDiff / (1000 * 60));
-      return `${minutesDiff} minutes`;
-    case 'seconds':
-      const secondsDiff = Math.floor(timeDiff / 1000);
-      return `${secondsDiff} seconds`;
-  }
+  const dimensionMap = {
+    days: 1000 * 60 * 60 * 24,
+    hours: 1000 * 60 * 60,
+    minutes: 1000 * 60,
+    seconds: 1000
+  };
+
+  const dimensionDiff = Math.floor(timeDiff / dimensionMap[dimension]);
+  return `${dimensionDiff} ${dimension}`;
 }
 
-console.log(durationBetweenDates('02 Aug 1985', '03 Aug 1985', 'seconds')) // поверне '86400 seconds'
-console.log(durationBetweenDates('31 Jan 2022', '03 Feb 2021', 'days')) // поверне '362 days'
+console.log(durationBetweenDates('02 Aug 1985', '03 Aug 1985', 'seconds')) // returns '86400 seconds'
+console.log(durationBetweenDates('31 Jan 2022', '03 Feb 2021', 'days')) // returns '362 days'
+
 
 // 2. Задача про перетворення об'єкту
 
@@ -52,9 +48,9 @@ console.log(updatedPriceData)    // {apples: '23.40', bananas: '48.00', oranges:
 function recursiveOddSumTo(number) {
   if (number <= 0) {
     return 0;
-  } else if (number % 2 === 0) {
+  } if (number % 2 === 0) {
     return recursiveOddSumTo(number - 1);
-  } else {
+  } {
     return number + recursiveOddSumTo(number - 2);
   }
 }
@@ -65,8 +61,8 @@ console.log(recursiveOddSumTo(10)) // 25
 
 // Ітерація:
 function iterativeOddSumTo(number) {
-    const sum = 0;
-    for (const i = 1; i <= number; i += 2) {
+    let sum = 0;
+    for (let i = 1; i <= number; i += 2) {
       sum += i;
     }
     return sum;
