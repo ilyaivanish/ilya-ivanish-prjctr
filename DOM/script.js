8d9bf6a397430e3eb6fdafb4339695b0e15685a7
Ilya Ivanishin <ilyaivanish@gmail.com>
2023-03-29 17:35:43 +0300

    dom-tree

--- DOM/script.js
+++ DOM/script.js
@@ -4,16 +4,19 @@ const lastChange = document.querySelector('#last-change');
 
 // Load the state of the button and the last change message from localStorage
 const isDark = localStorage.getItem('isDark') === 'true';
-const lastChangeMessage = localStorage.getItem('lastChangeMessage');
+const lastTime = localStorage.getItem('lastTime');
+
 if (isDark) {
   toggleBtn.textContent = 'Turn on';
   document.body.classList.add('dark');
+  lastChange.textContent = `Last turning off: ${lastTime}`;
 }
-if (lastChangeMessage) {
-  lastChange.textContent = lastChangeMessage;
+else {
+  lastChange.textContent = `Last turning on: ${lastTime}`;
 }
 
 // Add event listener to the button
+
 toggleBtn.addEventListener('click', () => {
   const lastTime = new Date().toLocaleString();
   let message;
@@ -22,17 +25,20 @@ toggleBtn.addEventListener('click', () => {
   if (toggleBtn.textContent === 'Turn off') {
     toggleBtn.textContent = 'Turn on';
     message = `Last turn off: ${lastTime}`;
-    localStorage.setItem('isDark', 'true');
+    localStorage.setItem('isDark', true);
+    
   } else {
     toggleBtn.textContent = 'Turn off';
     message = `Last turn on: ${lastTime}`;
-    localStorage.setItem('isDark', 'false');
+    localStorage.setItem('isDark', false);
   }
 
   // Toggle the background color
   document.body.classList.toggle('dark');
 
-  // Update the paragraph text and save it to localStorage
+  // Update the paragraph text
   lastChange.textContent = message;
-  localStorage.setItem('lastChangeMessage', message);
+
+  //save date to localStorage
+  localStorage.setItem('lastTime', lastTime);
 });
